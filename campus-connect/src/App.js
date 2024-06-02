f4fc93618ca46a2f9d57f916e3ebd127a3f29c61
Nithiya5 <nithiyaprabha.r2022@sece.ac.in>
2024-06-02 19:15:43 +0530

    setup

--- campus-connect/src/App.js
+++ campus-connect/src/App.js
@@ -21,8 +21,8 @@
 
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Register from './Register'; // Adjust the import path as needed
-import Login from './Login'; // Adjust the import path as needed
+import Register from './Register'; 
+import Login from './Login'; 
  import './App.css';
 import HomePage from './HomePage';
 import AboutUs from './Aboutus';
