5f17acdc77405991315e0d8ed21634919dcb4c5f
Nithiya5 <nithiyaprabha.r2022@sece.ac.in>
2024-06-08 07:35:29 +0530

    hpome

--- campus-connect/src/Home.js
+++ campus-connect/src/Home.js
@@ -280,7 +280,7 @@ const Home = () => {
           }
           .main-container {
             position: relative;
-            z-index: 1;
+            z-index: -1;
             width: 100%;
             height: 100%;
             display: flex;
